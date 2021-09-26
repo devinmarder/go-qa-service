7758eb45b6c89c4d923712eccf5bc8ba1d76afef
Devin Marder <devin.marder@gmail.com>
2021-09-26 17:43:29 +0200

    handler setup

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type ServiceStats struct {
+	ServiceName string `json:"service_name"`
+	Coverage    string `json:"coverage"`
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	//<extract service and coverage stats>
+	//<write service and coverage stats to repository>
+	fmt.Fprintf(w, "updated service")
+}
+
+func webHandler(w http.ResponseWriter, r *http.Request) {
+	//<get list of services and their coverages from repository>
+	//<generate html formatted response>
+	fmt.Fprintf(w, "<h1>list of services and their coverage</h1>")
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	//<get list of services and their coverages from repository>
+	//<generate json encoded response>
+	fmt.Fprint(w, `{"services": [{"service_name": "example_name", "coverage": 23}]`)
+}
+
+func main() {
+	port := os.Args[1]
+	http.HandleFunc("/", updateHandler)
+	http.HandleFunc("/stats", webHandler)
+	http.HandleFunc("/api/stats", apiHandler)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
