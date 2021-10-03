979bd1b21b83cc2dc48a65b6270ab8369d799078
Devin Marder <devin.marder@gmail.com>
2021-10-03 13:10:34 +0200

    dynamodb repository added

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/devinmarder/go-qa-service
 
 go 1.17
+
+require (
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.2.1
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.5.1
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.9.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.0.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.4.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.1.1 // indirect
+	github.com/aws/smithy-go v1.8.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
--- repository/repository.go
+++ repository/repository.go
@@ -1,20 +1,49 @@
 package repository
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
 type ServiceCoverage struct {
 	ServiceName string  `json:"service_name"`
 	Coverage    float32 `json:"coverage"`
 }
 
 type Repository interface {
-	UpdateServiceCoverage(serviceName string, coverage float32)
+	UpdateServiceCoverage(serviceName string, coverage float32) error
 	ListServiceCoverage() []ServiceCoverage
+}
+
+type DynamodbRepository struct {
+	tableName string
+	cl        dynamodb.Client
+}
+
+func (dr *DynamodbRepository) UpdateServiceCoverage(sn string, cov float32) (err error) {
+	_, err = dr.cl.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		Item: map[string]types.AttributeValue{
+			"service_name": &types.AttributeValueMemberS{Value: sn},
+			"coverage":     &types.AttributeValueMemberN{Value: fmt.Sprintf("%.2f", cov)}},
+		TableName: &dr.tableName})
+	return
+}
+
+func (dr *DynamodbRepository) ListServiceCoverage() (sc []ServiceCoverage) {
+	scan, _ := dr.cl.Scan(context.TODO(), &dynamodb.ScanInput{TableName: &dr.tableName})
+	attributevalue.UnmarshalListOfMaps(scan.Items, sc)
+	return
 }
 
 type LocalRepository struct {
 	Services []ServiceCoverage
 }
 
-func (lr *LocalRepository) UpdateServiceCoverage(sn string, cov float32) {
+func (lr *LocalRepository) UpdateServiceCoverage(sn string, cov float32) (err error) {
 	for i, v := range lr.Services {
 		if v.ServiceName == sn {
 			lr.Services[i].Coverage = cov
@@ -23,8 +52,10 @@ func (lr *LocalRepository) UpdateServiceCoverage(sn string, cov float32) {
 	}
 	newServiceCoverage := ServiceCoverage{ServiceName: sn, Coverage: cov}
 	lr.Services = append(lr.Services, newServiceCoverage)
+	return
 }
 
-func (lr *LocalRepository) ListServiceCoverage() []ServiceCoverage {
-	return lr.Services
+func (lr *LocalRepository) ListServiceCoverage() (sc []ServiceCoverage) {
+	sc = lr.Services
+	return
 }
